7699a3da956ebe0a1785e3c670da2d00ea1d1bb9
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2020-03-19 04:27:38 +0200

    Mobile pre login & post login

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import "./style.css";
 import "./responsive.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { withRouter } from "react-router";
-
+import SideMenu from "../SideMenu/SideMenu";
 import logoutIcon from "../../assets/image/logoutIcon.svg";
 import messageIcon from "../../assets/image/messageIcon.svg";
 import toggleIcon from "../../assets/image/toggleIcon.svg";
@@ -11,75 +11,116 @@ import infoIcon from "../../assets/image/infoIcon.svg";
 import { Layout, Menu, Divider } from "antd";
 const { Header } = Layout;
 
-const Navbar = props => {
-  return (
-    <>
-      <Layout className="header-layout">
-        <Header className="header">
-          <div className="logo">
-            <a href="#" className="logo-text">
-              DhanWise
-            </a>
-          </div>
-          <Menu
-            className="navbar-menu"
-            mode="horizontal"
-            style={{ lineHeight: "64px" }}
-          >
-            <Menu.Item key="1" className="clients">
-              <a href="#" className="nav-clients">
-                Clients
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+    this.handleActiveChange = this.handleActiveChange.bind(this);
+  }
+  state = {
+    isActive: false
+  };
+  handleActiveChange = isActive => {
+    this.setState({ isActive: isActive });
+  };
+
+  handleShow = () => {
+    this.setState({
+      isActive: true
+    });
+  };
+  handleHide = () => {
+    this.setState({
+      isActive: false
+    });
+  };
+
+  render() {
+    let show_class = this.state.isActive ? "sideMenuNav" : null;
+
+    return (
+      <>
+        <Layout className="header-layout">
+          <Header className="header">
+            <div className="logo">
+              <a href="#" className="logo-text">
+                DhanWise
               </a>
-            </Menu.Item>
-            <Menu.Item key="2" className="messages">
-              <div className="messages-wrapper">
-                <span className="messages-number-wrapper">
-                  <span className="messages-number">1</span>
-                </span>
-                <img src={messageIcon} />
-              </div>
-            </Menu.Item>
-            <Menu.Item key="3" className="divider-wrapper">
-              <Divider type="vertical" className="navmenu-divider" />
-            </Menu.Item>
-            <Menu.Item key="4">
-              <div className="logout-wrapper">
-                <a href="#" className="logout-text">
-                  Logout
-                </a>
-                <img src={logoutIcon} alt="" />
-              </div>
-            </Menu.Item>
-          </Menu>
-        </Header>
-      </Layout>
-      <div className="mobile-nav">
-        <div className="toggle-icon">
-          <img src={toggleIcon} alt="" />
-          <span className="mobile-messages-number-wrapper">
-            <span className="mobile-messages-number">3</span>
-          </span>
-        </div>
-        <Router>
-          <div
-            className="mobile-advisor-data"
-            onClick={() => props.history.push("/AdvisorDetailsMobile")}
-          >
-            <div className="name-img">
-              <img
-                src="https://pbs.twimg.com/profile_images/974736784906248192/gPZwCbdS.jpg"
-                alt=""
-                className="mobile-advisor-img"
-              />
-              <span className="mobile-advisor-name">Anantha Raman</span>
             </div>
-            <img src={infoIcon} alt="" className="mobile-infoIcon" />
+            <Menu
+              className="navbar-menu"
+              mode="horizontal"
+              style={{ lineHeight: "64px" }}
+            >
+              <Menu.Item key="1" className="clients">
+                <a href="#" className="nav-clients">
+                  Clients
+                </a>
+              </Menu.Item>
+              <Menu.Item key="2" className="messages">
+                <div className="messages-wrapper">
+                  <span className="messages-number-wrapper">
+                    <span className="messages-number">1</span>
+                  </span>
+                  <img src={messageIcon} />
+                </div>
+              </Menu.Item>
+              <Menu.Item key="3" className="divider-wrapper">
+                <Divider type="vertical" className="navmenu-divider" />
+              </Menu.Item>
+              <Menu.Item key="4">
+                <div className="logout-wrapper">
+                  <a href="#" className="logout-text">
+                    Logout
+                  </a>
+                  <img src={logoutIcon} alt="" />
+                </div>
+              </Menu.Item>
+            </Menu>
+          </Header>
+        </Layout>
+        <div className="mobile-nav">
+          <div className="sideNav " className={show_class}>
+            {this.state.isActive ? (
+              <SideMenu
+                isActive={this.state.isActive}
+                onChange={this.handleActiveChange}
+              />
+            ) : null}
           </div>
-        </Router>
-      </div>
-    </>
-  );
-};
+          <div className="toggle-icon">
+            <img
+              src={toggleIcon}
+              alt=""
+              onClick={() => {
+                this.handleShow();
+              }}
+            />
+            <span className="mobile-messages-number-wrapper">
+              <span className="mobile-messages-number">3</span>
+            </span>
+          </div>
+          <Router>
+            <div
+              className="mobile-advisor-data"
+              onClick={() => this.props.history.push("/AdvisorDetailsMobile")}
+            >
+              <div className="name-img">
+                <img
+                  src="https://pbs.twimg.com/profile_images/974736784906248192/gPZwCbdS.jpg"
+                  alt=""
+                  className="mobile-advisor-img"
+                />
+                <span className="mobile-advisor-name">Anantha Raman</span>
+              </div>
+              <img src={infoIcon} alt="" className="mobile-infoIcon" />
+            </div>
+          </Router>
+        </div>
+      </>
+    );
+  }
+}
 
 export default withRouter(Navbar);
-// export default Navbar;
---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,227 @@
+import React from "react";
+import "./style.css";
+import { Menu } from "antd";
+
+import closeSideMenuIcon from "../../assets/image/sideMenuIcons/closeSideMenuIcon.svg";
+import homeSideMenuIcon from "../../assets/image/sideMenuIcons/homeSideMenuIcon.svg";
+import suggestMutualFunds from "../../assets/image/sideMenuIcons/suggestMutualFunds.svg";
+import QuestionAndAnswerForum from "../../assets/image/sideMenuIcons/QuestionAndAnswerForum.svg";
+import learnIcon from "../../assets/image/sideMenuIcons/learnIcon.svg";
+import toolsIcon from "../../assets/image/sideMenuIcons/toolsIcon.svg";
+import aboutDhanWiseIcon from "../../assets/image/sideMenuIcons/aboutDhanWiseIcon.svg";
+import questions from "../../assets/image/sideMenuIcons/questions.svg";
+import phoneIcon from "../../assets/image/sideMenuIcons/phoneIcon.svg";
+import msgsNumber from "../../assets/image/sideMenuIcons/msgsNumber.svg";
+
+const { SubMenu } = Menu;
+
+class SideMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  // submenu keys of first level
+  rootSubmenuKeys = ["sub1", "sub2", "sub4"];
+
+  state = {
+    isActive: this.props.isActive,
+    isLoggedIn: true,
+    openKeys: ["sub3"]
+  };
+
+  handleHide = () => {
+    this.setState({
+      isActive: !this.state.isActive
+    });
+    this.handleActiveChange();
+  };
+
+  handleActiveChange = () => {
+    this.props.onChange(!this.state.isActive);
+  };
+
+  onOpenChange = openKeys => {
+    const latestOpenKey = openKeys.find(
+      key => this.state.openKeys.indexOf(key) === -1
+    );
+    if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      this.setState({ openKeys });
+    } else {
+      this.setState({
+        openKeys: latestOpenKey ? [latestOpenKey] : []
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.isActive ? (
+          <Menu
+            mode="inline"
+            openKeys={this.state.openKeys}
+            onOpenChange={this.onOpenChange}
+            style={{ width: 256 }}
+            className="sideMenu-ul"
+          >
+            <Menu.Item key="1" className="sideMenu-li">
+              <div className="test">
+                <img
+                  src={closeSideMenuIcon}
+                  alt=""
+                  className="close-icon"
+                  onClick={this.handleHide.bind(this)}
+                />
+              </div>
+            </Menu.Item>
+            {this.state.isLoggedIn ? (
+              <Menu.Item key="2" className="sideMenu-li your-advisor">
+                <div
+                  className="menu-item-content-wrapper"
+                  id="your-advisor-wrapper"
+                >
+                  <div id="your-advisor">Your Advisor</div>
+                  <img
+                    src="https://pbs.twimg.com/profile_images/974736784906248192/gPZwCbdS.jpg"
+                    alt=""
+                    className="menu-item-icon"
+                    id="menu-item-icon-avdisor-img"
+                  />
+                  <span className="menu-item-text" id="sidemenu-advisor-name">
+                    Anantha Raman
+                  </span>
+                  <img
+                    src={msgsNumber}
+                    alt=""
+                    className="menu-item-icon"
+                    id="menu-item-icon-messages-number"
+                  />
+                  <span id="msg-number">1</span>
+                </div>
+              </Menu.Item>
+            ) : (
+              <Menu.Item key="2" className="sideMenu-li">
+                <div className="menu-item-content-wrapper">
+                  <img
+                    src={homeSideMenuIcon}
+                    alt=""
+                    className="menu-item-icon"
+                  />
+                  <span className="menu-item-text">Home</span>
+                </div>
+              </Menu.Item>
+            )}
+
+            <Menu.Item key="3" className="sideMenu-li">
+              <div className="menu-item-content-wrapper">
+                <img
+                  src={suggestMutualFunds}
+                  alt=""
+                  className="menu-item-icon"
+                />
+                {this.state.isLoggedIn ? (
+                  <span className="menu-item-text">Your Financial Plan</span>
+                ) : (
+                  <span className="menu-item-text">Suggest Mutual Funds</span>
+                )}
+              </div>
+            </Menu.Item>
+            <Menu.Item key="4" className="sideMenu-li">
+              <div className="menu-item-content-wrapper">
+                <img
+                  src={QuestionAndAnswerForum}
+                  alt=""
+                  className="menu-item-icon"
+                />
+                <span className="menu-item-text">Q&amp;A Forum</span>
+              </div>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <div className="menu-item-content-wrapper">
+                  <img src={learnIcon} alt="" className="menu-item-icon" />
+                  <span className="menu-item-text">Learn</span>
+                </div>
+              }
+              className="sideMenu-li sideMenu-subMenu"
+            >
+              <Menu.Item key="5" className="sideMenu-li-li">
+                <div className="submenu-item-content-wrapper">
+                  <span className="submenu-item-text">About DhanWise</span>
+                </div>
+              </Menu.Item>
+              <Menu.Item key="6" className="sideMenu-li-li">
+                <div className="submenu-item-content-wrapper">
+                  <span className="submenu-item-text">About DhanWise</span>
+                </div>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <div className="menu-item-content-wrapper">
+                  <img src={toolsIcon} alt="" className="menu-item-icon" />
+                  <span className="menu-item-text">Tools</span>
+                </div>
+              }
+              className="sideMenu-li sideMenu-subMenu"
+            >
+              <Menu.Item key="7" className="sideMenu-li-li">
+                <div className="submenu-item-content-wrapper">
+                  <span className="submenu-item-text">
+                    Case Study: Financial Planning for a 30 year old
+                  </span>
+                </div>
+              </Menu.Item>
+              <Menu.Item key="8" className="sideMenu-li-li">
+                <div className="submenu-item-content-wrapper">
+                  <span className="submenu-item-text">Mutual Fund Basics</span>
+                </div>
+              </Menu.Item>
+              <Menu.Item key="9" className="sideMenu-li-li">
+                <div className="submenu-item-content-wrapper">
+                  <span className="submenu-item-text">
+                    Life Insurance Basics
+                  </span>
+                </div>
+              </Menu.Item>
+              <Menu.Item key="10" className="sideMenu-li-li">
+                <div className="submenu-item-content-wrapper">
+                  <span className="submenu-item-text">
+                    Health Insurance Basics
+                  </span>
+                </div>
+              </Menu.Item>
+            </SubMenu>
+
+            <Menu.Item key="11" className="sideMenu-li">
+              <div className="menu-item-content-wrapper">
+                <img
+                  src={aboutDhanWiseIcon}
+                  alt=""
+                  className="menu-item-icon"
+                />
+                <span className="menu-item-text">About DhanWise</span>
+              </div>
+            </Menu.Item>
+            <hr className="side-menu-divider" />
+            <Menu.Item key="12" className="sideMenu-li">
+              <div className="menu-item-content-wrapper">
+                <img src={questions} alt="" className="menu-item-icon" />
+                <span className="menu-item-text"> questions@dhanwise.com</span>
+              </div>
+            </Menu.Item>
+            <Menu.Item key="13" className="sideMenu-li">
+              <div className="menu-item-content-wrapper">
+                <img src={phoneIcon} alt="" className="menu-item-icon" />
+                <span className="menu-item-text">+91 98199 81962</span>
+              </div>
+            </Menu.Item>
+          </Menu>
+        ) : null}
+      </>
+    );
+  }
+}
+
+export default SideMenu;
